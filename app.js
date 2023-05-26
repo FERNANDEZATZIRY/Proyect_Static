f055daccee512ac0bb999c87afcd5ebc28f8014f
FERNANDEZATZIRY <124528995+FERNANDEZATZIRY@users.noreply.github.com>
2023-05-26 09:50:04 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+// Importando Express
+import express from 'express';
+// Importando http-status
+import httpStatus from 'http-status';
+import path from 'path';
+
+// Importando el enrutador
+import adminRouter from './routes/admin.route.js';
+import shopRouter from './routes/shop.route.js';
+import error from './routes/error.js';
+
+// Importando ROOT_DIR
+import { ROOT_DIR } from './helpers/paths.js';
+
+// Creando la instancia de express que basicamente es un middleware
+const app = express();
+
+// Se registra el middleware del body-parser
+app.use(express.urlencoded({ extended: true }));
+
+// Se registra el Middleware para el servidor de archivos estaticos
+app.use(express.static(path.join(ROOT_DIR, 'public')));
+
+// Se agrega ruta de administrador
+app.use('/admin', adminRouter);
+// Se agrega ruta shop
+app.use(shopRouter);
+// Se agrega la ruta de error
+app.use(error);
+
+// Definiendo puertos
+const port = 3000;
+const ip = "0.0.0.0"
+
+// Arrancando el servidor
+app.listen(port, ip, () => {
+    console.log(`🦾 Sirviendo en http://localhost:${port}`);
+});
